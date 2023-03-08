60013e7e119946842d9cf6908f5575e2e5accae6
Krishna Hernandez <hernandezkrishna09@gmail.com>
2023-03-08 15:49:56 +0800

    first starbucks

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
-const btnShow = document.querySelector('.btn__show');
-const btnBack = document.querySelector('.btn__back');
-const body = document.querySelector('body');
-
-if(btnShow){
-    btnShow.addEventListener('click', () => {
-        body.classList.add('show');
+const kamote = document.querySelectorAll(".links__item h4")
+kamote.forEach((kamoteq) => {
+    kamoteq.addEventListener("click", () => {
+        kamoteq.nextElementSibling.classList.toggle("open");
+        kamoteq.querySelector("i").classList.toggle("open");
     })
-}
+})
 
-if(btnBack){
-    btnBack.addEventListener('click', () => {
-        body.classList.remove('show');
-    })
-}
+
+
+
+
+// const btnShow = document.querySelector('.btn__show');
+// const btnBack = document.querySelector('.btn__back');
+// const body = document.querySelector('body');
+
+// if(btnShow){
+//     btnShow.addEventListener('click', () => {
+//         body.classList.add('show');
+//     })
+// }
+
+// if(btnBack){
+//     btnBack.addEventListener('click', () => {
+//         body.classList.remove('show');
+//     })
+// }
